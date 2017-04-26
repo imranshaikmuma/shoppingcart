b99d2a4040f7166fe9209f26606600b662bdb03b
imranshaikmuma <imranshaik@mail.usf.edu>
2017-04-26 19:41:57 -0400

    shopping cart application

---
+++ seed/product-seeder.js
@@ -0,0 +1,43 @@
+var Product = require('../models/product');
+var mongoose =require('mongoose');
+mongoose.connect('localhost:27017/shopping');
+
+
+
+var products = [
+	new Product({
+		imagePath:'https://support.apple.com/library/content/dam/edam/applecare/images/en_US/iphone/iphone-6s-colors.jpg',
+		title:'I Phone 7 plus',
+		description:'Awesome phone',
+		price:700
+}),
+	new Product({
+		imagePath:'http://68.media.tumblr.com/c29516f66e665ca492886713fefd3092/tumblr_n2xjc7W4A31qa6obyo2_1280.jpg',
+		title:'The wolf of wall street',
+		description:'Awesome movie',
+		price:99
+}),
+	new Product({
+		imagePath:'https://static.sportskeeda.com/wp-content/uploads/2016/07/cricket-1469195007.jpg',
+		title:'Cricket',
+		description:'Awesome game',
+		price:150
+})
+];
+
+var done=0;
+for (var i=0;i< products.length;i++){
+		products[i].save(function(err, result){
+			done++;
+			if (done==products.length){
+				exit();
+			}
+		});
+}
+
+
+
+function exit(){
+	mongoose.disconnect();
+}
+
