a3245152a7e825a4bc7edaa4267c122ac4348ca8
imranshaikmuma <imranshaik@mail.usf.edu>
2017-04-26 19:49:54 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,49 +1,60 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var expressHbs = require('express-handlebars');
-var mongoose =require('mongoose');
+var http      = require('http');
+var mongoose  = require('mongoose');
+var express   = require('express');
+var app    = express();
+var db;
+
+var config = {
+      "USER"    : "",           
+      "PASS"    : "",
+      "HOST"    : "ec2-34-209-113-86.us-west-2.compute.amazonaws.com",  
+      "PORT"    : "27017", 
+      "DATABASE" : "my_example"
+    };
+
+var dbPath  = "mongodb://"+config.USER + ":"+
+    config.PASS + "@"+
+    config.HOST + ":"+
+    config.PORT + "/"+
+    config.DATABASE;
+var standardGreeting = 'Hello World!';
 
 
+var greetingSchema = mongoose.Schema({
+  sentence: String
+}); 
+var Greeting= mongoose.model('Greeting', greetingSchema);
 
-var index = require('./routes/index');
+db = mongoose.connect(dbPath);
 
 
-var app = express();
-mongoose.connect('localhost:27017/shopping');
-// view engine setup
-app.engine('.hbs',expressHbs({defaultLayout:'layout',extname:'.hbs'}));
-app.set('view engine', 'hbs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+mongoose.connection.once('open', function() {
+  var greeting;
+  Greeting.find( function(err, greetings){
+   if( !greetings ){     
+      greeting = new Greeting({ sentence: standardGreeting }); 
+      greeting.save();
+    } 
+  }); 
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get('/', function(req, res){
+  Greeting.findOne(function (err, greeting) {
+    res.send(greeting.sentence);
+  });
 });
 
-module.exports = app;
+app.use(function(err, req, res, next){
+  if (req.xhr) {
+    res.send(500, 'Something went wrong!');
+  }
+  else {
+    next(err);
+  }
+});
+
+console.log('starting the Express (NodeJS) Web server');
+app.listen(8080);
+console.log('Webserver is listening on port 8080');
+
