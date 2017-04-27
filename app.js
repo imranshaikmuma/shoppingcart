41d82e9d0f8c5aad7273a9b837cf66768847b6fc
imranshaikmuma <imranshaik@mail.usf.edu>
2017-04-26 20:28:07 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var mongoose  = require('mongoose');
 var express   = require('express');
 var app    = express();
 var db;
-
 var config = {
       "USER"    : "",           
       "PASS"    : "",
@@ -11,50 +10,75 @@ var config = {
       "PORT"    : "27017", 
       "DATABASE" : "my_example"
     };
-
 var dbPath  = "mongodb://"+config.USER + ":"+
     config.PASS + "@"+
     config.HOST + ":"+
     config.PORT + "/"+
     config.DATABASE;
 var standardGreeting = 'Hello World!';
+//var greetingSchema = mongoose.Schema({
+//  sentence: String
+//}); 
+//var Greeting= mongoose.model('Greeting', greetingSchema);
 
+//db = mongoose.connect(dbPath);
+mongoose.connect(dbPath);
+//mongoose.connection.once('open', function() {
+//  var greeting;
+// Greeting.find( function(err, greetings){
+//   if( !greetings ){     
+//     greeting = new Greeting({ sentence: standardGreeting }); 
+//      greeting.save();
+//    } 
+//  }); 
+//});
+//app.get('/', function(req, res){
+//  Greeting.findOne(function (err, greeting) {
+//    res.send(greeting.sentence);
+//  });
+//});
+//app.use(function(err, req, res, next){
+//  if (req.xhr) {
+//    res.send(500, 'Something went wrong!');
+//  }
+//  else {
+//    next(err);
+//  }
+//});
+//console.log('starting the Express (NodeJS) Web server');
+//app.listen(8080);
+//console.log('Webserver is listening on port 8080');
+app.engine('.hbs',expressHbs({defaultLayout:'layout',extname:'.hbs'}));
+app.set('view engine', 'hbs');
 
-var greetingSchema = mongoose.Schema({
-  sentence: String
-}); 
-var Greeting= mongoose.model('Greeting', greetingSchema);
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-db = mongoose.connect(dbPath);
+app.use('/', index);
 
-
-mongoose.connection.once('open', function() {
-  var greeting;
-  Greeting.find( function(err, greetings){
-   if( !greetings ){     
-      greeting = new Greeting({ sentence: standardGreeting }); 
-      greeting.save();
-    } 
-  }); 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-app.get('/', function(req, res){
-  Greeting.findOne(function (err, greeting) {
-    res.send(greeting.sentence);
-  });
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-app.use(function(err, req, res, next){
-  if (req.xhr) {
-    res.send(500, 'Something went wrong!');
-  }
-  else {
-    next(err);
-  }
-});
+module.exports = app;
 
-console.log('starting the Express (NodeJS) Web server');
-app.listen(8080);
-console.log('Webserver is listening on port 8080');
 
